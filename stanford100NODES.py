c8cafa18b401d0f0c438ede25dcc4588150527ce
Matias Quinteros <matias.quinteros@usach.cl>
2015-12-09 18:35:02 -0300

    pagerank de los 10 primeros

--- stanford100NODES.py
+++ stanford100NODES.py
@@ -9,10 +9,10 @@ total = re.search(r"Nodes: (\d+) Edges: (\d+)",read)
 
 #total = re.search(r"(\d+) (\d+)",read)
 
-G1 = snap.TUNGraph.New()
+G1 = snap.TNGraph.New()
 nodos= int(total.group(1))
 edges=int(total.group(2))
-for x in range(1,nodos+1):
+for x in range(1,1001):
 	G1.AddNode(x)
 
 
@@ -28,14 +28,15 @@ for line in f:
 	pares = re.search(r"(\d+)\t(\d+)",an)
 	n1= int(pares.group(1))
 	n2=int(pares.group(2))
-	G1.AddEdge(n1,n2)
-
+	if(n1<=1000 and n2<=1000):
+		G1.AddEdge(n1,n2)
+		#print n1
+		#print n2
 
 #Genera un txt con los nodos
 #snap.SaveEdgeList(G1, "test.txt", "Save as tab-separated list of edges")
 cf = snap.GetClustCf(G1)
 print cf
-
 PRankH = snap.TIntFltH()
 snap.GetPageRank(G1, PRankH)
 item = []
@@ -47,12 +48,9 @@ for itema in PRankH:
 	temp.append(i+1)
  	item.append(temp)
  	i=i+1
-item.sort(reverse=True)
-i=11
-j=0
+item.sort()
 for valor in item:
-	j++
-	if(j<i):
-		print valor
-	else:
-		break
+	print valor
+#snap.DrawGViz(G1, snap.gvlDot, "graph100.png", "graph 100")
+#snap.DrawGViz(G1, snap.gvlDot, "output.png", " ", labels)
+#snap.DrawGViz(G1, snap.gvlDot, "grafostanford.png", "graph 1", True, NIdColorH)
